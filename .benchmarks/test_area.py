aa3121aa406e30f5a7ea19340b67b362b65028be
Wai-Shing Luk <luk036@gmail.com>
2025-08-29 12:29:55 +0000

    apply ruff format

--- .benchmarks/test_area.py
+++ .benchmarks/test_area.py
@@ -1,4 +1,3 @@
-
 import pytest
 from physdes.point import Point
 from physdes.polygon import Polygon
@@ -10,15 +9,18 @@ RECTILINEAR_POLYGON_COORDS = [
     (5, 5),
 ]
 
+
 @pytest.fixture
 def rectilinear_polygon_points():
     return [Point(x, y) for x, y in RECTILINEAR_POLYGON_COORDS]
+
 
 def test_polygon_signed_area(benchmark, rectilinear_polygon_points):
     rp = RPolygon(rectilinear_polygon_points)
     p: Polygon = rp.to_polygon()
     result = benchmark(lambda: p.signed_area_x2)
     assert abs(result) == 150
+
 
 def test_rpolygon_signed_area(benchmark, rectilinear_polygon_points):
     rp = RPolygon(rectilinear_polygon_points)
