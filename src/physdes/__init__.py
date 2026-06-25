e594991ff100e4eaa537f860f44e9a77d5d66058
Wai-Shing Luk <luk036@gmail.com>
2026-06-25 15:23:51 +0800

    cleanup: remove dead code, drop PyScaffold boilerplate, clean configs

--- src/physdes/__init__.py
+++ src/physdes/__init__.py
@@ -6,16 +6,9 @@ Vector2, ManhattanArc) and algorithms for clock tree synthesis (DME), global
 routing, Steiner forest construction, and polygon decomposition.
 """
 
-import sys
-
-if sys.version_info[:2] >= (3, 8):
-    # TODO: Import directly (no need for conditional) when `python_requires = >= 3.9`
-    from importlib.metadata import PackageNotFoundError, version  # pragma: no cover
-else:
-    from importlib_metadata import PackageNotFoundError, version  # pragma: no cover
+from importlib.metadata import PackageNotFoundError, version
 
 try:
-    # Change here if project is renamed and does not equal the package name
     dist_name = "physdes-py"
     __version__ = version(dist_name)
 except PackageNotFoundError:  # pragma: no cover
